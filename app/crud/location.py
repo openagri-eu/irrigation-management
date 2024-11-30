d1b0363a285e9e7101142d90af604973841ce2f6
stefan.drobic <stefan.drobic@vizlore.com>
2024-11-30 16:59:09 +0100

    implemented wkt api for location support

--- app/crud/location.py
+++ app/crud/location.py
@@ -1,3 +1,7 @@
+from typing import List
+
+from sqlalchemy.orm import Session
+
 from crud.base import CRUDBase
 from models.location import Location
 from schemas import LocationCreate, LocationUpdate
@@ -5,7 +9,8 @@ from schemas import LocationCreate, LocationUpdate
 
 class CrudLocation(CRUDBase[Location, LocationCreate, LocationUpdate]):
 
-    pass
+    def get_all(self, db: Session) -> List[Location]:
+        return db.query(Location).all()
 
 
 location = CrudLocation(Location)
